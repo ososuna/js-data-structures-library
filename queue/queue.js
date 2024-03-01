c23bef0883795acf1bc9640384e38eb9029609f5
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-02-29 18:29:50 -0600

    queue js docs

--- queue/queue.js
+++ queue/queue.js
@@ -1,15 +1,29 @@
 class Queue {
+  /** 
+  * Creates a new instance of Queue.
+  * @summary Time complexity: O(1)
+  */
   constructor() {
     this.count = 0;
     this.lowestCount = 0;
     this.items = {};
   }
 
+  /** 
+  * Adds a new element at the back of the queue.
+  * @summary Time complexity: O(1)
+  * @param {any} element - Element to be added to the queue.
+  */
   enqueue(element) {
     this.items[this.count] = element;
     this.count++;
   }
 
+  /** 
+  * Removes the first element from the queue and returns it. If the queue is empty, returns undefined.
+  * @summary Time complexity: O(1)
+  * @return {any | undefined} The first element from the queue, or undefined if the queue is empty.
+  */
   dequeue() {
     if (this.isEmpty()) {
       return undefined;
@@ -20,6 +34,11 @@ class Queue {
     return firstElement;
   }
 
+  /** 
+  * Returns the first element from the queue without removing it. If the queue is empty, returns undefined.
+  * @summary Time complexity: O(1)
+  * @return {any | undefined} The first element from the queue, or undefined if the queue is empty.
+  */  
   peek() {
     if (this.isEmpty()) {
       return undefined;
@@ -27,20 +46,39 @@ class Queue {
     return this.items[this.lowestCount];
   }
 
+  /** 
+  * Returns true if the queue does not contain any elements, and false if the size of the queue is bigger than 0.
+  * @summary Time complexity: O(1)
+  * @return {boolean} True if the queue does not contain any elements, and false if the size of the queue is bigger than 0.
+  */  
   isEmpty() {
     return this.size() === 0;
   }
 
+  /** 
+  * Returns the number of elements in the queue.
+  * @summary Time complexity: O(1)
+  * @return {number} The number of elements in the queue.
+  */  
   size() {
     return this.count - this.lowestCount;
   }
 
+  /** 
+  * Removes all elements from the queue.
+  * @summary Time complexity: O(1)
+  */  
   clear() {
     this.count = 0;
     this.lowestCount = 0;
     this.items = {};
   }
 
+  /** 
+  * Returns a string representation of the queue.
+  * @summary Time complexity: O(n)
+  * @return {string} A string representation of the queue.
+  */
   toString() {
     if (this.isEmpty()) {
       return '';
