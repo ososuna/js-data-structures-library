e05bc3d33155d74dc2a6b46e9cd146e5574ab2fa
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-03-04 19:37:49 -0600

    get element at method

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -30,14 +30,23 @@ export default class LinkedList {
     if (index === 0) {
       this.head = current.next;
     } else {
-      let previous;
-      for (let i = 0; i < index; i++) {
-        previous = current;
-        current = current.next;
-      }
+      let previous = this.getElementAt(index-1);
+      current = previous.next;
       previous.next = current.next;
     }
     this.count--;
     return current.element;
   }
+
+  getElementAt(index) {
+    if (index < 0 || index >= this.count) {
+      return undefined;
+    }
+    let current = this.head;
+    for (let i = 0; i < index && current != null; i++) {
+      current = current.next;  
+    }
+    return current.element;
+  }
+
 }
