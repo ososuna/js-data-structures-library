b1ecf2d6b110612090d4cd51331f119abb202bac
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-03-19 20:28:49 -0600

    stack linked list js docs

--- linked-list/stack-linked-list.js
+++ linked-list/stack-linked-list.js
@@ -1,14 +1,28 @@
 import DoublyLinkedList from './doubly-linked-list.js';
 
 export default class StackLinkedList {
+  /** 
+  * Creates a new instance of StackLinkedList.
+  * @summary Time Complexity: O(1)
+  */
   constructor() {
     this.items = new DoublyLinkedList();
   }
 
+  /** 
+  * Adds a new element to the top of the stack.
+  * @summary Time Complexity: O(1)
+  * @param {any} element - The element to be added to the stack.
+  */
   push(element) {
     this.items.push(element);
   }
 
+  /** 
+  * Removes the top element from the stack and returns it.
+  * @summary Time Complexity: O(1)
+  * @return {any | undefined} - The top element of the stack or undefined if the stack is empty.
+  */
   pop() {
     if (this.isEmpty()) {
       return undefined;
@@ -16,6 +30,11 @@ export default class StackLinkedList {
     return this.items.removeAt(this.size() - 1);
   }
 
+  /** 
+  * Returns the top element of the stack without removing it.
+  * @summary Time Complexity: O(1)
+  * @return {any | undefined} - The top element of the stack or undefined if the stack is empty.
+  */
   peek() {
     if (this.isEmpty()) {
       return undefined;
@@ -23,18 +42,37 @@ export default class StackLinkedList {
     return this.items.getElementAt(this.size() - 1).element;
   }
 
+  /** 
+  * Returns true if the stack is empty, otherwise returns false.
+  * @summary Time Complexity: O(1)
+  * @return {boolean} - True if the stack is empty, otherwise false.
+  */
   isEmpty() {
     return this.items.isEmpty();
   }
 
+  /** 
+  * Returns the number of elements in the stack.
+  * @summary Time Complexity: O(1)
+  * @return {number} - The number of elements in the stack.
+  */
   size() {
     return this.items.size();
   }
 
+  /** 
+  * Removes all elements from the stack.
+  * @summary Time Complexity: O(1)
+  */  
   clear() {
     this.items.clear();
   }
 
+  /** 
+  * Returns a string representation of the stack.
+  * @summary Time Complexity: O(n)
+  * @return {string} - The string representation of the stack.
+  */
   toString() {
     return this.items.toString();
   }
