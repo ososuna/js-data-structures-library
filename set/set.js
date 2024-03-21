c0f1493601aa251d71722a5f3a6b005b6065287e
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-03-20 19:21:32 -0600

    set class implementation

---
+++ set/set.js
@@ -0,0 +1,37 @@
+class Set {
+  constructor() {
+    this.items = {};
+  }
+
+  has(element) {
+    return Object.prototype.hasOwnProperty.call(this.items, element);
+  }
+
+  add(element) {
+    if (this.has(element)) {
+      return false;
+    }
+    this.items[element] = element;
+    return true;
+  }
+
+  delete(element) {
+    if (this.has(element)) {
+      delete this.items[element];
+      return true;
+    }
+    return false;
+  }
+
+  clear() {
+    this.items = {};
+  }
+
+  size() {
+    return Object.keys(this.items).length;
+  }
+
+  values() {
+    return Object.values(this.items);
+  }
+}
