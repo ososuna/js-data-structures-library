4d54c5d4d7e4f1b951ef5a98ecb147cfe3f717fe
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-03-27 17:38:59 -0600

    set class js docs

--- set/set.js
+++ set/set.js
@@ -1,12 +1,28 @@
-class Set {
+export default class Set {
+  /** 
+  * Creates a new instance of Set
+  * @summary Time complexity: O(1)
+  */
   constructor() {
     this.items = {};
   }
 
+  /** 
+  * Returns true if the set contains the specified element, and false otherwise.
+  * @summary Time complexity: O(1)
+  * @param {any} element - Element to be checked.
+  * @return {boolean} True if the set contains the specified element, and false otherwise.
+  */  
   has(element) {
     return Object.prototype.hasOwnProperty.call(this.items, element);
   }
 
+  /** 
+  * Adds a new element to the set.
+  * @summary Time complexity: O(1)
+  * @param {any} element - Element to be added to the set.
+  * @return {boolean} True if the element was added successfully, and false if the element is already in the set.
+  */
   add(element) {
     if (this.has(element)) {
       return false;
@@ -15,6 +31,12 @@ class Set {
     return true;
   }
 
+  /** 
+  * Removes the specified element from the set.
+  * @summary Time complexity: O(1)
+  * @param {any} element - Element to be removed from the set.
+  * @return {boolean} True if the element was removed successfully, and false if the element is not in the set.
+  */
   delete(element) {
     if (this.has(element)) {
       delete this.items[element];
@@ -23,14 +45,28 @@ class Set {
     return false;
   }
 
+  /** 
+  * Removes all elements from the set.
+  * @summary Time complexity: O(1)
+  */
   clear() {
     this.items = {};
   }
 
+  /** 
+  * Returns the number of elements in the set.
+  * @summary Time complexity: O(n)
+  * @return {number} The number of elements in the set.
+  */  
   size() {
     return Object.keys(this.items).length;
   }
 
+  /** 
+  * Returns an array containing all elements in the set.
+  * @summary Time complexity: O(n)
+  * @return {any[]} An array containing all elements in the set.
+  */
   values() {
     return Object.values(this.items);
   }
